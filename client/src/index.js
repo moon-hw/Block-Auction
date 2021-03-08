71d0203671bb257b71fb0791ea29b14c158feb0f
dojyj <dojyj@naver.com>
2021-03-08 12:38:18 +0900

    version 충돌방지 test #1

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// redux
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers/rootReducers';
+import thunk from 'redux-thunk';
+
+// firebase
+import firebase from 'firebase/app';
+import fbConfig from './firebase.utils';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+
+const store = createStore(
+    rootReducer,
+    compose(applyMiddleware(thunk))
+);
+
+// react-redux-firebase 데이터베이스 접근 설정
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/store/actions/ActionTypes.js
@@ -0,0 +1,4 @@
+export const CREATE_AUCTION = 'CREATE_AUCTION';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
