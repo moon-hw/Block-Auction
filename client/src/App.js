72585eddb1862a0e61f3a9564c93dc247711fcbd
dojyj <dojyj@naver.com>
2021-03-08 23:38:58 +0900

    TOP AUCTION 공간 배정

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import Home from "./Home";
 import NotFound from "./NotFound";
 import Auction from "./Auction";
 import Header from "./Header";
 import NoticeBoard from "./NoticeBoard";
+import TopAuction from "./TopAuction";
 
 function App() {
   return (
     <Router>
       <Header />
       <NoticeBoard />
+      <TopAuction />
       <main>
         <Switch>
           <Route exact path="/" component={Home} />
---
+++ client/src/TopAuction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TopAuction extends React.Component {
+    render(){
+        return (
+            <div className="Top">
+
+            </div>
+        );
+    }
+}
+
+export default TopAuction;
