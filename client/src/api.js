7e03f24b8581e408b15d1fb50859fea5e932545f
dojyj <dojyj@naver.com>
2021-04-07 15:38:33 +0900

    ha

--- client/src/api.js
+++ client/src/api.js
@@ -57,5 +57,5 @@ export const userApi = {
 export const auctionApi = {
     getAuctionList: (body) => api.post("/auctions/getauctionlist"),
     postAuction: (body) => api.post("/users/postauction", body),
-    postImage:(body) => api.post("/auction/postimage", body),
+    postImage:(body) => api.post("/auctions/postimage", body),
 }
